4b8e83736b013c74c0887601cfbebdceda942776
D1-Devesh-83855 <deveshsharma372002@gmail.com>
2024-10-09 16:37:16 +0530

    worked on Service section and about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Services from "./components/Services/Services";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Hero />
         <About />
+        <Services />
       </div>
       Hy this is App.jsx
     </>
